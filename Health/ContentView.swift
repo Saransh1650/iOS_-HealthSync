2835c80c2a9892a4373de85bf5873fb6c9d77e34
Saransh Singhal <singhalsaransh40@gmail.com>
2024-06-13 03:14:29 +0530

    Initial Commit

---
+++ Health/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Health
+//
+//  Created by Saransh Singhal on 13/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Health/HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthApp.swift
+//  Health
+//
+//  Created by Saransh Singhal on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
